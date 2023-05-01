3a60be382e2212da8cbc0c1269e750ab2069aa6a
JuHeonHa <hjhjaueon97@naver.com>
2023-05-02 01:16:24 +0900

    로그인 토큰

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,8 @@ import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import { userLogin } from "../api/user";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { tokenState } from "../state/token";
 
 const Container = styled.div`
   width: 100%;
@@ -23,16 +25,16 @@ const ButtonConainer = styled.div``;
 
 function Login() {
   const { handleSubmit, register, setValue } = useForm();
+  const [token, setToken] = useRecoilState(tokenState);
   const navigate = useNavigate();
   const onClickLogin = async (loginData: any) => {
-    const res = await userLogin(loginData).catch((fail) => {
-      console.log(fail);
+    const res: any = await userLogin(loginData).catch((fail) => {
       if (fail.response.status === 401) {
         alert("Login Fail");
       }
     });
     if (res) {
-      //console.log(res.token);
+      setToken(res.token);
       navigate("/");
     }
     setValue("username", "");
@@ -41,15 +43,12 @@ function Login() {
 
   return (
     <Container>
-      <div>
-        <p>mor_2314</p>
-        <p>83r5^_</p>
-      </div>
       <LoginForm onSubmit={handleSubmit(onClickLogin)}>
         <InputContainer>
           <input type="text" placeholder="ID" {...register("username", { required: true })} />
           <input type="password" placeholder="PW" {...register("password", { required: true })} />
         </InputContainer>
+
         <button type="submit">Login</button>
       </LoginForm>
     </Container>
